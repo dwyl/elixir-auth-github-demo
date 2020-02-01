52d643845977d42a9419c50bbaae586a57f75867
nelsonic <nelson+github@dwyl.io>
2020-02-01 21:56:04 +0000

    tidy up controllers (remove IO.inspect)

--- lib/app_web/controllers/github_auth_controller.ex
+++ lib/app_web/controllers/github_auth_controller.ex
@@ -5,9 +5,7 @@ defmodule AppWeb.GithubAuthController do
   `index/2` handles the callback from GitHub Auth API redirect.
   """
   def index(conn, %{"code" => code}) do
-    # {:ok, token} = ElixirAuthGithub.get_token(code, conn)
     {:ok, profile} = ElixirAuthGithub.github_auth(code)
-    IO.inspect profile, label: "profile"
     conn
     |> put_view(AppWeb.PageView)
     |> render(:welcome, profile: profile)
--- lib/app_web/controllers/page_controller.ex
+++ lib/app_web/controllers/page_controller.ex
@@ -2,9 +2,7 @@ defmodule AppWeb.PageController do
   use AppWeb, :controller
 
   def index(conn, _params) do
-    scopes = ["user", "user:email"]
-    oauth_github_url = ElixirAuthGithub.login_url_with_scope(scopes)
-    IO.inspect oauth_github_url
+    oauth_github_url = ElixirAuthGithub.login_url_with_scope(["user:email"])
     render(conn, "index.html", [oauth_github_url: oauth_github_url])
   end
 end
