7cb256c0e7eb2763108ee2debe70277267766ac2
nelsonic <nelson+github@dwyl.io>
2020-02-01 22:38:16 +0000

    add database config for heroku deployment (even though we arent using a DB to store any data!!

--- config/prod.exs
+++ config/prod.exs
@@ -69,3 +69,9 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
 # import_config "prod.secret.exs"
+# Configure your database
+config :app, App.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
