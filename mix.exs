cda7936cecde2714e1c5ce2e682954d20e60bbb9
nelsonic <nelson+github@dwyl.io>
2020-02-01 22:15:06 +0000

    update version of phoenix

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule App.MixProject do
   def project do
     [
       app: :app,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: "1.0.2",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -33,7 +33,7 @@ defmodule App.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.0"},
+      {:phoenix, "~> 1.4.12"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.0"},
