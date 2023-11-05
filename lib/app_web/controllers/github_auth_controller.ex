420a2b5fe8262f7039e441ce3f17cd9394ecfa96
nelsonic <nelson+github@dwyl.com>
2023-11-05 05:16:16 +0000

    define github_auth_controller.ex #29

---
+++ lib/app_web/controllers/github_auth_controller.ex
@@ -0,0 +1,11 @@
+defmodule AppWeb.GithubAuthController do
+  use AppWeb, :controller
+
+  @doc """
+  `index/2` handles the callback from GitHub Auth API redirect.
+  """
+  def index(conn, %{"code" => code}) do
+    {:ok, profile} = ElixirAuthGithub.github_auth(code)
+    render(conn, :welcome, [layout: false, profile: profile])
+  end
+end
